f4b595423ed49831841d0412134576034fcf6c23
KryssNa <krishna.kryss@gmail.com>
2023-07-22 16:18:25 +0545

    file added

--- src/main/java/com/example/AniMall/controller/CheckoutController.java
+++ src/main/java/com/example/AniMall/controller/CheckoutController.java
@@ -2,16 +2,20 @@ package com.example.AniMall.controller;
 
 import com.example.AniMall.Entity.Cart;
 import com.example.AniMall.Pojo.BookingPojo;
+import com.example.AniMall.Pojo.ShippingDetailsDto;
+import com.example.AniMall.Repo.SummaryDetailsRepo;
+import com.example.AniMall.Services.BookingServices;
 import com.example.AniMall.Services.CartServices;
 import com.example.AniMall.Services.UserServices;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
@@ -21,9 +25,12 @@ import java.util.List;
 public class CheckoutController {
     private final UserServices userService;
     private final CartServices cartService;
+    private final BookingServices bookingServices;
+    final SummaryDetailsRepo summaryDetailsRepo ;
+//    private final SummaryDetailsServices summaryDetailsServices;
 
     @PostMapping("/checkout")
-    public String displayCart(Principal principal, Model model){
+    public String checkkout(Principal principal, Model model){
         Integer id = userService.findByEmail(principal.getName()).getId();
         List<Cart> list = cartService.fetchAll(id);
 
@@ -41,16 +48,37 @@ public class CheckoutController {
         return "redirect:/checkout/confirm";
     }
 
-    @GetMapping("/checkout/confirm")
-    public String checkoutProcess(Principal principal, @Valid BookingPojo pojo){
-        Integer id = userService.findByEmail(principal.getName()).getId();
-        List<Cart> list = cartService.fetchAvailable(id);
-
-        for (Cart cartItem: list){
-            cartService.updatePet(cartItem.getPet().getQuantity()-cartItem.getQuantity(),cartItem.getPet().getId());
+    @PostMapping("/checkout/confirm")
+    public String checkoutProcess(Principal principal, @Valid BookingPojo pojo, @Valid ShippingDetailsDto shippingDetailsDto, BindingResult bindingResult) throws IOException {
+        if (bindingResult.hasErrors()) {
+            // If there are validation errors, redirect back to the checkout page to display the errors
+            System.out.println("error"+bindingResult.getAllErrors());
+            return "redirect:/cart";
         }
 
-        cartService.checkout(id, pojo, list);
+        // Get the currently logged-in user's ID
+        Integer id = userService.findByEmail(principal.getName()).getId();
+
+        // Fetch the cart items for the user
+        List<Cart> list = cartService.fetchAvailable(id);
+
+        // Update the available quantity of pets after checkout
+        for (Cart cartItem : list) {
+            cartService.updatePet(cartItem.getPet().getQuantity() - cartItem.getQuantity(), cartItem.getPet().getId());
+        }
+
+        System.out.println("checkout process");
+        // Perform the checkout process using the cartService
+        cartService.checkout(id, pojo, shippingDetailsDto, list);
+
+        System.out.println("summary details");
+        // Perform the checkout process using the cartService
+//        bookingServices.summaryCheckout(id,pojo, shippingDetailsDto);
+
+        // Clear the user's cart after successful checkout
+//        cartService.clearCart(id);
+
+        // Redirect the user to the homepage after successful checkout
         return "redirect:/user/homepage";
     }
 }
