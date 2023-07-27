97d1edfba439ab8c6ba774470421f763a9ffda8b
KryssNa <krishna.kryss@gmail.com>
2023-07-27 14:56:36 +0545

    dead code removed

--- src/main/java/com/example/AniMall/AniMallApplication.java
+++ src/main/java/com/example/AniMall/AniMallApplication.java
@@ -5,19 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AniMallApplication {
-//
-//    @Autowired
-//    private EmailSenderService service;
-
     public static void main(String[] args) {
         SpringApplication.run(AniMallApplication.class, args);
     }
-//
-//    @EventListener(ApplicationReadyEvent.class)
-//    public void triggerMail(){
-//        service.sendSimpleEmail("asmitakatel444@gmail.com","hI","tHIS IS EMAIL");
-//    }
-//
-
-
 }
